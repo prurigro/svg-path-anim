8e45c78f1c237d9073f55359070613f6dd92fb6c
Nataliya Sayenko <nataliyasayenko@gmail.com>
2017-01-24 10:59:20 -0500

    regex

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ Array.from(links).forEach(link => {
 
 
 function hexToRgb(path) {
-	const points = path.getAttribute('fill').substring(1);
-	const bigint = parseInt(points, 16);
+    const points = path.getAttribute('fill').substring(1);
+    const bigint = parseInt(points, 16);
     const r = (bigint >> 16) & 255;
     const g = (bigint >> 8) & 255;
     const b = bigint & 255;
@@ -31,32 +31,32 @@ const toPathsArray = [];
 const fromPathsArray =[];
 
 function getPaths(animateTo) {
-	const toPaths = document.getElementById(animateTo).querySelectorAll('path');
+    const toPaths = document.getElementById(animateTo).querySelectorAll('path');
 
-	Array.from(toPaths).forEach((path, i) => {
+    Array.from(toPaths).forEach((path, i) => {
 
-	   const d = toPaths[i].getAttribute("d");
-	   pathObj.fill = toPaths[i].getAttribute("fill");
-	   pathObj.x0 = d.match(/\M(.*)\,/)[1];
-	   pathObj.y0 = d.match(/\,(.*)\L/)[1];
-	   L = d.substring(d.indexOf("L")+1).split(" ");
-	   pathObj.L0 = L[0];
-	   pathObj.L1 = L[1];
-	   pathObj.L2 = L[2];
-	   pathObj.join = L[3].slice(0, -1);
+       const d = toPaths[i].getAttribute("d");
+       pathObj.fill = toPaths[i].getAttribute("fill");
+       pathObj.x0 = d.match(/\M(.*)\,/)[1];
+       pathObj.y0 = d.match(/\,(.*)\L/)[1];
 
-	   	toPathsArray.push(pathObj);
+        toPathsArray.push(pathObj);
+       let one, two, three, four;
 
-	});
+        d.replace(/L([0-9][0-9]*)\ ([0-9][0-9]*)\ ([0-9][0-9]*)\ ([0-9][0-9]*)z/, (match, $1, $2, $3, $4) => { one = $1; two = $2; three = $3; four = $4; return match; });
 
-		
-	 Array.from(document.querySelector(".visible").querySelectorAll("path")).forEach((path, i) => {
+        console.log(four)
+
+    });
+
+        
+     Array.from(document.querySelector(".visible").querySelectorAll("path")).forEach((path, i) => {
 
 
-	 	// TweenLite.to(fromPathObj, 1, { x0: pathObj.x0, onUpdate: function() {
+        // TweenLite.to(fromPathObj, 1, { x0: pathObj.x0, onUpdate: function() {
    //          path.setAttribute("d",`M${pathObj.x0},${pathObj.y0}L${pathObj.L0} ${pathObj.L1} ${pathObj.L2} ${pathObj.join}z`);
    //      } });
-	 });
+     });
 
 }
 
