2d8521d2fefb04c6d14887c43b1407972de43a87
Nataliya Sayenko <nataliyasayenko@gmail.com>
2017-01-24 13:08:57 -0500

    aniamte

--- script.js
+++ script.js
@@ -23,8 +23,6 @@ function hexToRgb(path) {
     path.setAttribute("fill", `rgba( ${color}, 0.5)`);
 }
 
-const pathObj = {};
-const fromPathObj = {};
 const toPathsArray = [];
 const fromPathsArray = [];
 
@@ -32,6 +30,8 @@ function getPaths(animateTo) {
     const toPaths = document.getElementById(animateTo).querySelectorAll("path");
 
     Array.from(toPaths).forEach((path, i) => {
+        const pathObj = {};
+
         const coordinates = toPaths[i].getAttribute("d").match(/M(-?[0-9][0-9]*),(-?[0-9][0-9]*)L(-?[0-9][0-9]*)\ (-?[0-9][0-9]*)\ (-?[0-9][0-9]*)\ (-?[0-9][0-9]*)z/);
 
         pathObj.fill = toPaths[i].getAttribute("fill");
@@ -41,16 +41,39 @@ function getPaths(animateTo) {
         pathObj.L1 = coordinates[4];
         pathObj.L2 = coordinates[5];
         pathObj.join = coordinates[6];
-        toPathsArray.push(pathObj);
+        fromPathsArray.push(pathObj);
 
         // console.log(toPaths[i].getAttribute("d"))
         // console.log(pathObj.join)
     });
 
     Array.from(document.querySelector(".visible").querySelectorAll("path")).forEach((path, i) => {
-        // TweenLite.to(fromPathObj, 1, { x0: pathObj.x0, onUpdate: function() {
-   //          path.setAttribute("d",`M${pathObj.x0},${pathObj.y0}L${pathObj.L0} ${pathObj.L1} ${pathObj.L2} ${pathObj.join}z`);
-   //      } });
+        const coordinates = path.getAttribute("d").match(/M(-?[0-9][0-9]*),(-?[0-9][0-9]*)L(-?[0-9][0-9]*)\ (-?[0-9][0-9]*)\ (-?[0-9][0-9]*)\ (-?[0-9][0-9]*)z/);
+        const toPathObj = {};
+
+        toPathObj.fill = toPaths[i].getAttribute("fill");
+        toPathObj.x0 = coordinates[1];
+        toPathObj.y0 = coordinates[2];
+        toPathObj.L0 = coordinates[3];
+        toPathObj.L1 = coordinates[4];
+        toPathObj.L2 = coordinates[5];
+        toPathObj.join = coordinates[6];
+
+        toPathsArray.push(toPathObj);
     });
+
+    animatePaths();
+
+    function animatePaths() {
+        toPathsArray.forEach((obj, i) => {
+            console.log(obj.x0)
+            console.log(fromPathsArray[i].x0)
+            TweenLite.to(obj, 1, {x0:fromPathsArray[i].x0, y0:fromPathsArray[i].y0, L0:fromPathsArray[i].L0, L1:fromPathsArray[i].L1, L2:fromPathsArray[i].L2, join:fromPathsArray[i].join, ease: Power4.easeOut, onUpdate: function() {
+
+                document.querySelector(".visible").querySelectorAll("path")[i].setAttribute("d" , `M${obj.x0},${obj.y0}L${obj.L0} ${obj.L1} ${obj.L2} ${obj.join}z`);
+            } }); 
+        });
+    }
 }
+
 
