78d6bd982058cfb9181054f07176bb5dd8c08613
Nataliya Sayenko <nataliyasayenko@gmail.com>
2017-01-23 23:30:01 -0500

    idk

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const paths = document.querySelectorAll('path');
-const natPaths = document.querySelectorAll('#nat path');
-
 const links = document.querySelectorAll('a');
 
 [].forEach.call(paths,hexToRgb);
 
-natPaths.forEach(function(path) {
-	animatePaths(path);
-})
+
+Array.from(links).forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const animateTo = this.getAttribute("href").substring(1);
+
+        getPaths(animateTo);
+    });
+});
+
 
 function hexToRgb(path) {
 	const points = path.getAttribute('fill').substring(1);
@@ -20,7 +25,38 @@ function hexToRgb(path) {
     path.setAttribute("fill", `rgba( ${color}, 0.5)`);
 }
 
+const pathObj = {};
+const fromPathObj = {};
+const toPathsArray = [];
+const fromPathsArray =[];
 
-function animatePaths(path) {
+function getPaths(animateTo) {
+	const toPaths = document.getElementById(animateTo).querySelectorAll('path');
+
+	Array.from(toPaths).forEach((path, i) => {
+
+	   const d = toPaths[i].getAttribute("d");
+	   pathObj.fill = toPaths[i].getAttribute("fill");
+	   pathObj.x0 = d.match(/\M(.*)\,/)[1];
+	   pathObj.y0 = d.match(/\,(.*)\L/)[1];
+	   pathObj.L = d.substring(d.indexOf("L")+1).split(" ");
+	   pathObj.L0 = pathObj.L[0];
+	   pathObj.L1 = pathObj.L[1];
+	   pathObj.L2 = pathObj.L[2];
+	   pathObj.join = pathObj.L[3].slice(0, -1);
+	   
+	   	toPathsArray.push(pathObj);
+
+	});
+
+		
+	 Array.from(document.querySelector(".visible").querySelectorAll("path")).forEach((path, i) => {
+
+
+	 	// TweenLite.to(fromPathObj, 1, { x0: pathObj.x0, onUpdate: function() {
+   //          path.setAttribute("d",`M${pathObj.x0},${pathObj.y0}L${pathObj.L0} ${pathObj.L1} ${pathObj.L2} ${pathObj.join}z`);
+   //      } });
+	 });
 
 }
+
