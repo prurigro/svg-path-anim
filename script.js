4ba9dec675dbad7b78a697d9f8e786fdaf256013
Nataliya Sayenko <nataliyasayenko@gmail.com>
2017-01-24 16:50:56 -0500

    stuff

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const links = document.querySelectorAll("a");
-const toPathsArray = [];
-const fromPathsArray = [];
+let toPathsArray = [];
+let fromPathsArray = [];
+let animateTo ="nat";
 
 // click on link listener
 Array.from(links).forEach(link => {
     link.addEventListener("click", function(event) {
         event.preventDefault();
         const animateTo = this.getAttribute("href").substring(1);
-
         getPaths(animateTo);
     });
 });
@@ -17,71 +17,55 @@ const getCoordinates = function(path) {
     return path.getAttribute("points").match(/(-?[0-9][0-9\.]*),(-?[0-9][0-9\.]*)\ (-?[0-9][0-9\.]*),(-?[0-9][0-9\.]*)\ (-?[0-9][0-9\.]*),(-?[0-9][0-9\.]*)/);
 };
 
-function animatePaths() {
-    // animate d attr
-    const polygons = Array.from(document.querySelector(".svg-holder").querySelectorAll("polygon"));
+function getPathsArray(object) {
+    const pathsArray = [];
 
-    toPathsArray.forEach((obj, i) => {
-        TweenLite.to(obj, 2, { x0: fromPathsArray[i].x0, y0: fromPathsArray[i].y0, L0: fromPathsArray[i].L0, L1: fromPathsArray[i].L1, L2: fromPathsArray[i].L2, join: fromPathsArray[i].join, ease: Power4.easeOut, onUpdate: function() {
-            polygons[i].setAttribute("points", `${obj.x0},${obj.y0} ${obj.L0},${obj.L1} ${obj.L2},${obj.join}`);
-        } });
-    });
-    
-    // animate color
-    Array.from(document.querySelector(".visible").querySelectorAll("polygon")).forEach((path, i) => {
-        const fromColor = path.getAttribute("fill");
-        const toColor = toPathsArray[i].fill;
-
-        TweenMax.to(path, 2, { fill: toColor });
-    });
-}
-
-// add d attribute values to arrays
-function getPaths(animateTo) {
-    const toPaths = document.getElementById(animateTo).querySelectorAll("polygon");
-
-    Array.from(toPaths).forEach((path, i) => {
+    Array.from(object).forEach((path, i) => {
+        const coordinates = getCoordinates(path);
         const pathObj = {};
 
-        const coordinates = getCoordinates(toPaths[i]);
-
-        pathObj.fill = toPaths[i].getAttribute("fill");
+        pathObj.fill = path.getAttribute("fill");
         pathObj.x0 = coordinates[1];
         pathObj.y0 = coordinates[2];
         pathObj.L0 = coordinates[3];
         pathObj.L1 = coordinates[4];
         pathObj.L2 = coordinates[5];
         pathObj.join = coordinates[6];
-        fromPathsArray.push(pathObj);
+
+        pathsArray.push(pathObj);
     });
+   // console.log(pathsArray[0])
 
-    Array.from(document.querySelector(".visible").querySelectorAll("polygon")).forEach((path, i) => {
-        const coordinates = getCoordinates(path);
-        const toPathObj = {};
-
-        toPathObj.fill = toPaths[i].getAttribute("fill");
-        toPathObj.x0 = coordinates[1];
-        toPathObj.y0 = coordinates[2];
-        toPathObj.L0 = coordinates[3];
-        toPathObj.L1 = coordinates[4];
-        toPathObj.L2 = coordinates[5];
-        toPathObj.join = coordinates[6];
-
-        toPathsArray.push(toPathObj);
-    });
-
-    animatePaths();
+    return pathsArray;
 }
 
-// [].forEach.call(paths, hexToRgb);
-// function hexToRgb(path) {
-//     const points = path.getAttribute("fill").substring(1);
-//     const bigint = parseInt(points, 16);
-//     const r = (bigint >> 16) & 255;
-//     const g = (bigint >> 8) & 255;
-//     const b = bigint & 255;
-//     const color = r + "," + g + "," + b;
+function animatePaths() {
+    // animate d attr
+    const polygons = Array.from(document.querySelector(".svg-holder").querySelectorAll("polygon"));
+    console.log(fromPathsArray[0])
+    console.log(toPathsArray[0])
 
-//     path.setAttribute("fill", `rgb( ${color})`);
-// }
+    fromPathsArray.forEach((obj, i) => {
+        TweenLite.to(obj, 2, { x0: toPathsArray[i].x0, y0: toPathsArray[i].y0, L0: toPathsArray[i].L0, L1: toPathsArray[i].L1, L2: toPathsArray[i].L2, join: toPathsArray[i].join, ease: Power4.easeOut, onUpdate: function() {
+            polygons[i].setAttribute("points", `${obj.x0},${obj.y0} ${obj.L0},${obj.L1} ${obj.L2},${obj.join}`);
+        } });
+    });
+    
+    // animate color
+    Array.from(document.querySelector(".svg-holder").querySelectorAll("polygon")).forEach((path, i) => {
+        const fromColor = fromPathsArray[i].fill;
+        const toColor = toPathsArray[i].fill;
 
+        TweenMax.to(path, 2, { fill: toColor });
+    });
+}
+
+fromPathsArray = getPathsArray(document.querySelector(".svg-holder").querySelectorAll("polygon"));
+
+// add d attribute values to arrays
+function getPaths(animateTo) {
+    toPathsArray = getPathsArray(document.getElementById(animateTo).querySelectorAll("polygon"));
+
+    animatePaths();
+    fromPathsArray = toPathsArray;
+}
