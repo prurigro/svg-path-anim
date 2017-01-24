2c0626b444436f31d56954a8cee275075f62c9ee
Nataliya Sayenko <nataliyasayenko@gmail.com>
2017-01-23 20:02:06 -0500

    stuff

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-var polys = document.querySelectorAll('path');
-[].forEach.call(polys,changehex);
+const paths = document.querySelectorAll('path');
+const natPaths = document.querySelectorAll('#nat path');
 
-function changehex(poly) {
-	var points = poly.getAttribute('fill').substring(1);
-	var bigint = parseInt(points, 16);
-    var r = (bigint >> 16) & 255;
-    var g = (bigint >> 8) & 255;
-    var b = bigint & 255;
+const links = document.querySelectorAll('a');
+
+[].forEach.call(paths,hexToRgb);
+
+natPaths.forEach(function(path) {
+	animatePaths(path);
+})
+
+function hexToRgb(path) {
+	const points = path.getAttribute('fill').substring(1);
+	const bigint = parseInt(points, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
     const color = r + "," + g + "," + b;
 
-    poly.setAttribute("fill", "rgba(" + color + ", 0.5)")
+    path.setAttribute("fill", `rgba( ${color}, 0.5)`);
 }
 
 
+function animatePaths(path) {
+
+}
